c93763bb553bc0860d9f61ada193c75989ba12e3
Aditya Sinha <adityahjp11111@gmail.com>
2026-09-11 00:05:38 +0530

    base class for all SQLAlchemy models

---
+++ backend/app/database/base.py
@@ -0,0 +1,5 @@
+from sqlalchemy.orm import DeclarativeBase
+
+# Base class for all SQLAlchemy models, providing a foundation for defining database tables and their relationships.
+class Base(DeclarativeBase):
+    pass
