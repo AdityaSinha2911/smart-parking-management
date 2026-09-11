f6f35bfaaddcd5aeb50e373a120ffff04df47ad5
Aditya Sinha <adityahjp11111@gmail.com>
2026-09-11 08:03:20 +0530

    model packages added to initializer file

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,5 @@
+# intitialize the models package and import the necessary model classes
+from app.models.user import User
+from app.models.vehicle import Vehicle
+from app.models.parking_slot import ParkingSlot
+from app.models.booking import Booking
